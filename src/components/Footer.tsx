0d5add8d399e6adda0c9afaa3ac86b23f9932eae
eivcl <diana.pontes.ext@bayer.com>
2021-08-08 20:05:56 -0300

    fix links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "../../styles/components/Footer.module.css";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <a href="https://instagram.com/trans_dev/" target="_blank">
+      <a href="https://instagram.com/trans_dev/" target="_blank" rel="noreferrer">
         <img
           src="https://img.shields.io/badge/instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white"
           alt="instagram"
@@ -11,14 +11,14 @@ export default function Footer() {
       </a>
       <a
         href="https://youtube.com/channel/UCrvkNV7byf3t4QLzM5TVxfA"
-        target="_blank"
+        target="_blank" rel="noreferrer"
       >
         <img
           src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white"
           alt="youtube"
         />
       </a>{" "}
-      <a href="https://github.com/dianamartine" target="_blank">
+      <a href="https://github.com/dianamartine" target="_blank" rel="noreferrer">
         <img
           src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"
           alt="github"
